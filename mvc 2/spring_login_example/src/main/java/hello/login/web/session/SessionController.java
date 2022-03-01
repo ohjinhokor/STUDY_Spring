b90d34bdb21423759ebe753421eb52019b96ce5c
ohjinhokor <ohjinho7@gmail.com>
2022-03-01 11:41:21 +0900

    로그인 - 세션 정보와 타임아웃 설정

---
+++ mvc 2/spring_login_example/src/main/java/hello/login/web/session/SessionController.java
@@ -0,0 +1,33 @@
+package hello.login.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.time.DateTimeException;
+import java.util.Date;
+
+@Slf4j
+public class SessionController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "세션이 없습니다.";
+        }
+
+        //세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name)));
+
+        log.info("sessionId = {}", session.getId()); //jsessionId(쿠키에 담겨서 오는 값) 출력
+        log.info("getMaxInactiveInterval = {}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", new Date(session.getCreationTime()));
+        log.info("lastAccessTime ={}", new Date(session.getLastAccessedTime()));
+        log.info("isNew={}", session.isNew());
+
+        return "세션 출력";
+    }
+}
