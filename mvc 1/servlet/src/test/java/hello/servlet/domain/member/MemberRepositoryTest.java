f7f4f891064b8e66172a5bf95c8dabec282bbca0
bepi <ohjinho7@gmail.com>
2022-03-14 23:40:17 +0900

    MVC 프레임워크 만들기 - View 분리 - v2

--- mvc 1/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ mvc 1/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,13 +1,11 @@
 package hello.servlet.domain.member;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class MemberRepositoryTest {
@@ -38,9 +36,10 @@ class MemberRepositoryTest {
         Member member1 = new Member("member1", 20);
         Member member2 = new Member("member2", 30);
 
-        //when
         memberRepository.save(member1);
         memberRepository.save(member2);
+
+        //when
         List<Member> result = memberRepository.findAll();
 
         //then
