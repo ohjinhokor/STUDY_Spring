bb2b29c80a9feed7c96cfd40a0e08420069154bc
bepi <ohjinho7@gmail.com>
2022-03-19 23:48:04 +0900

    MVC 프레임워크 만들기 - 유연한 컨트롤러2 - v5

--- mvc 1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ mvc 1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,18 +34,24 @@ public class FrontControllerServletV5 extends HttpServlet {
         initHandlerAdapters();
     }
 
-    private boolean initHandlerAdapters() {
-        return handlerAdapters.add(new ControllerV3HandlerAdapter());
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     private void initHandlerMappingMap() {
-        this.handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        this.handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        this.handlerMappingMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        this.handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        this.handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        this.handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        this.handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        this.handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        this.handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         Object handler = getHandler(request);
         if (handler == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -52,7 +62,9 @@ public class FrontControllerServletV5 extends HttpServlet {
         ModelView mv = adapter.handle(request, response, handler);
 
         String viewName = mv.getViewName();
-        MyView myView = viewResolver(viewName);
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
     }
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
