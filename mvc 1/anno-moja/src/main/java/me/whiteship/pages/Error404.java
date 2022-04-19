1b07b565fafdb882aa0726b838715569d12e4db0
ohjinhokor <ohjinho7@gmail.com>
2022-04-19 23:04:58 +0900

    프로젝트 생성

---
+++ mvc 1/anno-moja/src/main/java/me/whiteship/pages/Error404.java
@@ -0,0 +1,4 @@
+package me.whiteship.pages;
+
+public class Error404 {
+}
---
+++ mvc 1/anno-moja/src/test/java/me/whiteship/tapestry/unit/pages/IndexTest.java
@@ -0,0 +1,28 @@
+package me.whiteship.tapestry.unit.pages;
+
+import org.apache.tapestry5.dom.Document;
+import org.apache.tapestry5.test.PageTester;
+import org.junit.jupiter.api.Test;
+
+import me.whiteship.services.AppModule;
+import me.whiteship.services.QaModule;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IndexTest {
+
+    @Test
+    void test() {
+        // given
+        String appPackage = "me.whiteship";
+        String appName = "App";
+        PageTester tester = new PageTester(appPackage, appName, "src/main/webapp", AppModule.class, QaModule.class);
+
+        // when
+        Document doc = tester.renderPage("Index");
+
+        // then
+        assertTrue(doc.getElementById("title").getChildMarkup().startsWith("Welcome to Tapestry 5!"));
+    }
+}
+
